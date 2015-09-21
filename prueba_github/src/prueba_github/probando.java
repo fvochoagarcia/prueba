d6728f1fc6e1b166133264cb45dce6c76ab79e8b
fvochoagarcia <fvochoagarcia@hotmail.com>
2015-09-20 21:43:01 -0500

    probando

---
+++ prueba_github/src/prueba_github/probando.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package prueba_github;
+
+/**
+ * @author Fernando Ochoa
+ *
+ */
+public class probando {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+
+	}
+
+}
